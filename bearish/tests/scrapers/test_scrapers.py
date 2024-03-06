8ba266990b966de0585199e02fd7d6a1ab94d4fd
aan <andoludovic.andriamamonjy@gmail.com>
2024-03-06 06:41:24 +0100

    fix: skip slow tests.

--- bearish/tests/scrapers/test_scrapers.py
+++ bearish/tests/scrapers/test_scrapers.py
@@ -9,25 +9,25 @@ from bearish.scrapers.main import DataSource, Scraper
 logger = logging.getLogger(__name__)
 
 
-@pytest.skip("Too slow")
+@pytest.mark.skip("Too slow")
 def test_investing_main_scraper() -> None:
     scraper = Scraper(source=DataSource.investing, country="belgium")
     scraper.scrape(skip_existing=False)
 
 
-@pytest.skip("Too slow")
+@pytest.mark.skip("Too slow")
 def test_investing_main_scraper_france() -> None:
     scraper = Scraper(source=DataSource.investing, country="france")
     scraper.scrape(skip_existing=False)
 
 
-@pytest.skip("Too slow")
+@pytest.mark.skip("Too slow")
 def test_investing_main_scraper_germany() -> None:
     scraper = Scraper(source=DataSource.investing, country="germany")
     scraper.scrape(skip_existing=False)
 
 
-@pytest.skip("Too slow")
+@pytest.mark.skip("Too slow")
 def test_investing_main_scraper_db_json() -> None:
     scraper = Scraper(source=DataSource.investing, country="belgium")
     db_json = scraper.create_db_json()
@@ -39,7 +39,7 @@ def test_investing_main_scraper_db_json() -> None:
         json.dump(db_json, p, indent=4)
 
 
-@pytest.skip("Too slow")
+@pytest.mark.skip("Too slow")
 def test_trading_main_scraper() -> None:
     scraper = Scraper(source=DataSource.trading, country="belgium")
     scraper.scrape()
