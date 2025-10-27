d16a948d8b23b47b9d0f342b97f280a3398fbfc9
aan <andoludovic.andriamamonjy@gmail.com>
2025-10-27 11:29:12 +0100

    fix: add price index

--- tests/sources/test_financedatabase.py
+++ tests/sources/test_financedatabase.py
@@ -6,12 +6,13 @@ from bearish.models.assets.equity import Equity
 from bearish.models.assets.crypto import Crypto
 from bearish.models.assets.currency import Currency
 from bearish.models.assets.etfs import Etf
+from bearish.models.assets.index import Index
 from bearish.sources.financedatabase import (
     RAW_EQUITIES_DATA_URL,
     FinanceDatabaseSource,
     RAW_CRYPTO_DATA_URL,
     RAW_CURRENCY_DATA_URL,
-    RAW_ETF_DATA_URL,
+    RAW_ETF_DATA_URL, RAW_INDEX_DATA_URL,
 )
 
 
@@ -45,9 +46,17 @@ def test_read_assets() -> None:
             .joinpath("data/sources/financedatabase/etfs.csv")
             .read_text(),
         )
+        req.get(
+            RAW_INDEX_DATA_URL,
+            text=Path(__file__)
+            .parents[1]
+            .joinpath("data/sources/financedatabase/index.csv")
+            .read_text(),
+        )
         assets = FinanceDatabaseSource()._read_assets()
         assert assets
         assert all(isinstance(equity, Equity) for equity in assets.equities)
         assert all(isinstance(crypto, Crypto) for crypto in assets.cryptos)
         assert all(isinstance(currency, Currency) for currency in assets.currencies)
         assert all(isinstance(etf, Etf) for etf in assets.etfs)
+        assert all(isinstance(index, Index) for index in assets.index)
