9ef9c837078da38ca308befa67d3fd861a898f84
aan <andoludovic.andriamamonjy@gmail.com>
2025-11-01 12:31:08 +0100

    fix: Add SEC

--- tests/test_sec_fillings.py
+++ tests/test_sec_fillings.py
@@ -4,9 +4,9 @@ from bearish.database.crud import BearishDb
 from bearish.models.sec.sec import Secs
 
 
-def test_sec(bear_db: BearishDb) -> None:
+def test_sec(bearish_db: BearishDb) -> None:
     secs = Secs.from_sec_13f_hr(
         "0001067983", date_=datetime.strptime("2024-10-01", "%Y-%m-%d").date()
     )
     assert secs.secs
-    secs.write(bear_db)
+    secs.write(bearish_db)
